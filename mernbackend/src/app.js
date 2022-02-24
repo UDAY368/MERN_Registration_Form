696a2286a7a7befef584a1b8c78f284429103e7e
K UDAY KUMAR <72476990+UDAY368@users.noreply.github.com>
2022-02-24 13:45:47 +0530

    Adding Patials to the app.js

--- mernbackend/src/app.js
+++ mernbackend/src/app.js
@@ -1,12 +1,18 @@
 const express = require('express');
 const path = require('path');
+const hbs = require('hbs');
 
 const app = express();
 require('./db/connect')
 
 const staticPath = path.join(__dirname, '../public');
+const templatePath = path.join(__dirname, '../templets/views');
+const partialsPath = path.join(__dirname, '../templets/partials');
+
 app.use(express.static(staticPath));
 app.set('view engine', 'hbs');
+app.set('views', templatePath);
+hbs.registerPartials(partialsPath);
 
 const port = process.env.PORT || 4000;
 
